2d23e7029a3a56506b65184a92f95da352b923ec
Zach Scott <zach@protocoltech.io>
2024-09-07 15:03:55 -0600

    base nvim config

---
+++ lua/plugins/elixir.lua
@@ -0,0 +1,115 @@
+return {
+  {
+    'elixir-tools/elixir-tools.nvim',
+    version = '*',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local elixir = require 'elixir'
+      local elixirls = require 'elixir.elixirls'
+
+      elixir.setup {
+        nextls = { enable = true },
+        elixirls = {
+          -- specify a repository and branch
+          -- repo = 'mhanberg/elixir-ls', -- defaults to elixir-lsp/elixir-ls
+          -- branch = 'mh/all-workspace-symbols', -- defaults to nil, just checkouts out the default branch, mutually exclusive with the `tag` option
+          -- tag = 'v0.14.6', -- defaults to nil, mutually exclusive with the `branch` option
+
+          -- alternatively, point to an existing elixir-ls installation (optional)
+          -- not currently supported by elixirls, but can be a table if you wish to pass other args `{"path/to/elixirls", "--foo"}`
+          -- cmd = '/usr/local/bin/elixir-ls.sh',
+
+          -- default settings, use the `settings` function to override settings
+          settings = elixirls.settings {
+            dialyzerEnabled = true,
+            fetchDeps = false,
+            enableTestLenses = true,
+            suggestSpecs = false,
+          },
+          on_attach = function(client, bufnr)
+            vim.keymap.set('n', '<space>fp', ':ElixirFromPipe<cr>', { buffer = true, noremap = true })
+            vim.keymap.set('n', '<space>tp', ':ElixirToPipe<cr>', { buffer = true, noremap = true })
+            vim.keymap.set('v', '<space>em', ':ElixirExpandMacro<cr>', { buffer = true, noremap = true })
+          end,
+        },
+        projectionist = {
+          enable = true,
+        },
+      }
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    { 'jfpedroza/neotest-elixir' },
+    {
+      'nvim-neotest/neotest',
+      dependencies = {
+        'nvim-neotest/nvim-nio',
+        'nvim-lua/plenary.nvim',
+        'antoinemadec/FixCursorHold.nvim',
+        'nvim-treesitter/nvim-treesitter',
+      },
+      config = function()
+        local neotest = require 'neotest'
+        neotest.setup {
+          log_level = vim.log.levels.DEBUG,
+          adapters = {
+            require 'neotest-elixir',
+          },
+        }
+        -- Keymaps
+        vim.keymap.set('n', '<leader>tt', function()
+          neotest.run.run()
+        end, { desc = 'Run nearest test' })
+
+        vim.keymap.set('n', '<leader>tf', function()
+          neotest.run.run(vim.fn.expand '%')
+        end, { desc = 'Run current file' })
+
+        vim.keymap.set('n', '<leader>ta', function()
+          neotest.run.run(vim.fn.getcwd())
+        end, { desc = 'Run all tests' })
+
+        vim.keymap.set('n', '<leader>ts', function()
+          neotest.summary.toggle()
+        end, { desc = 'Toggle test summary' })
+
+        vim.keymap.set('n', '<leader>to', function()
+          neotest.output.open { enter = true }
+        end, { desc = 'Show test output' })
+
+        vim.keymap.set('n', '<leader>tO', function()
+          neotest.output.open { enter = true, short = true }
+        end, { desc = 'Show test output (short)' })
+
+        vim.keymap.set('n', '<leader>tS', function()
+          neotest.run.stop()
+        end, { desc = 'Stop test run' })
+
+        vim.keymap.set('n', '<leader>tl', function()
+          neotest.run.run_last()
+        end, { desc = 'Run last test' })
+
+        vim.keymap.set('n', '[t', function()
+          neotest.jump.prev { status = 'failed' }
+        end, { desc = 'Jump to previous failed test' })
+
+        vim.keymap.set('n', ']t', function()
+          neotest.jump.next { status = 'failed' }
+        end, { desc = 'Jump to next failed test' })
+
+        vim.keymap.set('n', '<leader>tw', function()
+          neotest.watch.toggle(vim.fn.expand '%')
+        end, { desc = 'Watch current file' })
+
+        vim.keymap.set('n', '<leader>tW', function()
+          neotest.watch.toggle(vim.fn.getcwd())
+        end, { desc = 'Watch all tests' })
+
+        vim.keymap.set('n', '<leader>td', function()
+          neotest.run.run { strategy = 'dap' }
+        end, { desc = 'Debug nearest test' })
+      end,
+    },
+  },
+}
